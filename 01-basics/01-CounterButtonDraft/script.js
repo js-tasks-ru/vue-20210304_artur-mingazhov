5d8b48658adfd23322ae799915bdd1ce3d5baa24
Artur Mingazhov <arthurming7@gmail.com>
2021-03-17 17:32:19 +0500

    add solution for CounterButtonDraft

--- 01-basics/01-CounterButtonDraft/script.js
+++ 01-basics/01-CounterButtonDraft/script.js
@@ -1,3 +1,17 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+const app = new Vue({
+   el: '#app',
+   data() {
+      return {
+         counter: 0,
+      }
+   },
+   methods: {
+      increment() {
+         this.counter++;
+      }
+   }
+})
+
+app.$mount('#app');
--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,47 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+const app = new Vue({
+   el: '#app',
+   data() {
+      return {
+         picked: "",
+         arr: [],
+      }
+
+   },
+   async created() {
+
+      for (let i = 1; i < 6; i++) {
+         let us = await fetch(
+            `https://course-vue.javascript.ru/api/meetups/${i}`
+         );
+         let l = await us.json();
+         const t = {
+            id: l.id,
+            title: l.title,
+         }
+         this.arr.push(t)
+
+      }
+      /*
+          let us = await fetch(
+             `https://course-vue.javascript.ru/api/meetups/1`
+          );
+          this.arr = await us.json();
+    
+    */
+   },
+   methods: {
+      f() {
+         alert(this.arr.title)
+      },
+   },
+   computed: {
+      async a() {
+
+      }
+   }
+
+})
+
+app.$mount('#app');
