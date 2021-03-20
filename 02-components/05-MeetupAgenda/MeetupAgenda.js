8a17b6b32f1d659686df7c3f25acb3bcc38724a4
Artur Mingazhov <arthurming7@gmail.com>
2021-03-20 16:47:12 +0500

    add solution for 05-MeetupAgenda

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,13 +1,22 @@
-// import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default {
   name: 'MeetupAgenda',
+  components: {
+    MeetupAgendaItem,
+  },
+  props: {
+    agenda: {
+      type: Array,
+      required: true
+    }
+  },
 
   template: `
     <div class="meetup-agenda">
-      <div class="meetup-agenda__item"></div>
-      <div class="meetup-agenda__item"></div>
-      <!-- ... -->
-      <div class="meetup-agenda__item"></div>
+       <meetup-agenda-item :agendaItem="item" 
+       v-for="(item, indx) in agenda"
+       :key="indx"
+       />
     </div>`,
 };
