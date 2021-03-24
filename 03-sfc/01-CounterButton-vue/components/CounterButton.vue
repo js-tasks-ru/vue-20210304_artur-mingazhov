ec0f95049d4c6cfa586c9393c47ea7ea5d4eeb03
Artur Mingazhov <arthurming7@gmail.com>
2021-03-24 22:49:32 +0500

    add solution for 01-CounterButton-vue && 02-FormGroup

---
+++ 03-sfc/01-CounterButton-vue/components/CounterButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button type="button" @click="$emit(`increment`, count + 1)">{{ count }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CounterButton',
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style></style>
