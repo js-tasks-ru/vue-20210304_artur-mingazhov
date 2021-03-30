f3fdd84e9819ca77a0361cea1cb1cfd694014355
Artur Mingazhov <arthurming7@gmail.com>
2021-03-30 07:49:22 +0500

    добавляю model в DropdownButton

--- 03-sfc/04-DropdownButton-1/components/DropdownButton.vue
+++ 03-sfc/04-DropdownButton-1/components/DropdownButton.vue
@@ -1,22 +1,20 @@
-<!--вариант 1-->
-
 <template>
   <div class="dropdown" :class="{ show: show }" @click="show = !show">
     <button type="button" class="button dropdown__toggle" :class="{ dropdown__toggle_icon: hasIcon }">
-      <app-icon :icon="selected.icon" v-if="selected.icon !== undefined" />
+      <app-icon v-if="selected.icon !== undefined" :icon="selected.icon" />
       {{ title }} - {{ selected.text }}
     </button>
 
     <div class="dropdown__menu" :class="{ show: show }">
       <button
+        v-for="(option, index) in options"
+        :key="index"
         class="dropdown__item"
         :class="{ dropdown__item_icon: hasIcon }"
         type="button"
-        v-for="(option, index) in options"
-        :key="index"
         @click="$emit(`change`, option.value)"
       >
-        <app-icon :icon="option.icon" v-if="option.icon !== undefined" />
+        <app-icon v-if="option.icon !== undefined" :icon="option.icon" />
         {{ option.text }}
       </button>
     </div>
@@ -31,25 +29,34 @@ export default {
 
   components: { AppIcon },
 
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+
   props: {
     title: {
       type: String,
       required: true,
     },
+
     options: {
       type: Array,
       required: true,
     },
+
     value: {
       type: String,
       default: '',
     },
   },
+
   data() {
     return {
       show: false,
     };
   },
+
   computed: {
     selected() {
       if (this.value) {
@@ -59,81 +66,7 @@ export default {
     },
 
     hasIcon() {
-      for (let i = 0; i < this.options.length; i++) {
-        if (Object.prototype.hasOwnProperty.call(this.options[i], 'icon')) {
-          return true;
-        }
-      }
-      return false;
-    },
-  },
-  methods: {},
-};
-</script>
-
-<style src="@/assets/styles/_button.css" scoped></style>
-<style src="@/assets/styles/_dropdown.css" scoped></style>
-
-
-<!--вариант 2-->
-<!--
-<template>
-  <div class="dropdown" :class="{ show: show }" @click="show = !show">
-    <button type="button" class="button dropdown__toggle" :class="{ dropdown__toggle_icon: hasIcon }">
-      <app-icon :icon="selected.icon" v-if="selected.icon !== undefined" />
-      {{ title }} - {{ selected.text }}
-    </button>
-
-    <div class="dropdown__menu" :class="{ show: show }">
-      <button
-        class="dropdown__item"
-        :class="{ dropdown__item_icon: hasIcon }"
-        type="button"
-        v-for="(option, index) in options"
-        :key="index"
-        @click="$emit(`change`, option)"
-      >
-        <app-icon :icon="option.icon" v-if="option.icon !== undefined" />
-        {{ option.text }}
-      </button>
-    </div>
-  </div>
-</template>
-
-<script>
-import AppIcon from './AppIcon';
-
-export default {
-  name: 'DropdownButton',
-
-  components: { AppIcon },
-
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
-    options: {
-      type: Array,
-      required: true,
-    },
-    selected: {
-      type: Object,
-    },
-  },
-  data() {
-    return {
-      show: false,
-    };
-  },
-  computed: {
-    hasIcon() {
-      for (let i = 0; i < this.options.length; i++) {
-        if (Object.prototype.hasOwnProperty.call(this.options[i], 'icon')) {
-          return true;
-        }
-      }
-      return false;
+      return this.options.some((el) => Object.prototype.hasOwnProperty.call(el, 'icon'));
     },
   },
 };
@@ -141,5 +74,3 @@ export default {
 
 <style src="@/assets/styles/_button.css" scoped></style>
 <style src="@/assets/styles/_dropdown.css" scoped></style>
--->
-
