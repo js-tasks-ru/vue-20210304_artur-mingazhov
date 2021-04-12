346d5f1f0c51f5d733182c8fc3d0ff98e9e79aed
Artur Mingazhov <arthurming7@gmail.com>
2021-04-12 10:46:51 +0500

    add solution for 04-Layouts

--- 04-spa/04-Layouts/components/AuthLayout.vue
+++ 04-spa/04-Layouts/components/AuthLayout.vue
@@ -1,13 +1,21 @@
-<template></template>
+<template>
+  <div class="page">
+    <div class="page_onboarding">
+      <h2>{{ title }}</h2>
+      <slot />
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
   name: 'AuthLayout',
+  props: ['title'],
 };
 </script>
 
 <style scoped>
-.page.page_onboarding {
+.page .page_onboarding {
   max-width: 374px;
   width: 100%;
   margin: 0 auto;
--- 04-spa/04-Layouts/components/FormLayout.vue
+++ 04-spa/04-Layouts/components/FormLayout.vue
@@ -1,18 +1,26 @@
-<template></template>
+<template>
+  <div class="page">
+    <h2>{{ title }}</h2>
+    <div class="page_meetup-forms">
+      <slot />
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
   name: 'FormLayout',
+  props: ['title'],
 };
 </script>
 
 <style scoped>
-.page.page_meetup-forms {
+.page .page_meetup-forms {
   padding: 70px 0;
 }
 
 @media all and (max-width: 992px) {
-  .page.page_meetup-forms {
+  .page .page_meetup-forms {
     padding: 48px 0;
   }
 }
