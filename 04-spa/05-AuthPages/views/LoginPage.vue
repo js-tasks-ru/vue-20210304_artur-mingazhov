069441a339767fc864b5f31f3774b6d232949f7a
Artur Mingazhov <arthurming7@gmail.com>
2021-04-13 15:26:14 +0500

    add solution for 05-AuthPages

--- 04-spa/05-AuthPages/views/LoginPage.vue
+++ 04-spa/05-AuthPages/views/LoginPage.vue
@@ -1,15 +1,15 @@
 <template>
-  <form class="form">
+  <form class="form" @submit.prevent="submitForm">
     <div class="form-group">
       <label class="form-label">Email</label>
       <div class="input-group">
-        <input type="email" placeholder="demo@email" class="form-control" />
+        <input v-model="user.email" type="email" placeholder="demo@email" class="form-control" />
       </div>
     </div>
     <div class="form-group">
       <label class="form-label">Пароль</label>
       <div class="input-group">
-        <input type="password" placeholder="password" class="form-control" />
+        <input v-model="user.password" type="password" placeholder="password" class="form-control" />
       </div>
     </div>
     <div class="form__buttons">
@@ -17,16 +17,48 @@
     </div>
     <div class="form__append">
       Нет аккаунта?
-      <a class="link">Зарегистрируйтесь</a>
+      <router-link to="/register" class="link">Зарегистрируйтесь</router-link>
     </div>
   </form>
 </template>
 
 <script>
-// import { login } from '../data';
+import { login } from '../data';
 
 export default {
   name: 'LoginPage',
+
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+
+  methods: {
+    submitForm() {
+      if (this.user.email === '') {
+        alert('Требуется ввести Email');
+        return;
+      } else if (this.user.password === '') {
+        alert('Требуется ввести пароль');
+        return;
+      }
+
+      login(this.user.email, this.user.password).then((res) => {
+        if (res.fullname !== undefined) {
+          alert(res.fullname);
+          if (this.$route.query.from !== undefined) {
+            this.$router.push(this.$route.query.from);
+          } else {
+            this.$router.push('/');
+          }
+        } else alert(res.message);
+      });
+    },
+  },
 };
 </script>
 
