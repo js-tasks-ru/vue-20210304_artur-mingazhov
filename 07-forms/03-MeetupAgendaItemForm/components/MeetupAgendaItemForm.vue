061e5497342ccdf399fd17cb84ca14177264d566
Artur Mingazhov <arthurming7@gmail.com>
2021-04-28 10:49:28 +0500

    add solution for 07-forms/03-MeetupAgendaItemForm

--- 07-forms/03-MeetupAgendaItemForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/03-MeetupAgendaItemForm/components/MeetupAgendaItemForm.vue
@@ -1,48 +1,73 @@
 <template>
   <div class="form-section form-section_inner">
-    <button type="button" class="remove-button">
+    <button type="button" class="remove-button" @click="$emit('remove')">
       <app-icon icon="trash" />
     </button>
 
-    <div class="form-group">
-      <select class="form-control" title="Тип">
-        <option value="talk">Доклад</option>
-        <option value="other">Другое</option>
+    <form-group>
+      <select v-model="localAgendaItem.type" class="form-control" title="Тип">
+        <option v-for="(item, index) in agendaItemTypes" :key="index" :value="item.value">{{ item.text }}</option>
       </select>
-    </div>
+    </form-group>
 
     <div class="form__row">
       <div class="form__col">
-        <div class="form-group">
-          <label class="form-label">Начало</label>
-          <input class="form-control" type="time" placeholder="00:00" />
-        </div>
+        <form-group label="Начало">
+          <date-input v-model="startsAt" type="time" placeholder="00:00" />
+        </form-group>
       </div>
       <div class="form__col">
-        <div class="form-group">
-          <label class="form-label">Окончание</label>
-          <input class="form-control" type="time" placeholder="00:00" />
-        </div>
+        <form-group label="Окончание">
+          <date-input v-model="endsAt" type="time" placeholder="00:00" />
+        </form-group>
       </div>
     </div>
 
-    <div class="form-group">
-      <label class="form-label">Заголовок</label>
-      <input class="form-control" />
-    </div>
-    <div class="form-group">
-      <label class="form-label">Описание</label>
-      <textarea class="form-control"></textarea>
-    </div>
-    <div class="form-group">
-      <label class="form-label">Язык</label>
-      <select class="form-control"></select>
-    </div>
+    <template v-if="localAgendaItem.type == 'other'">
+      <form-group label="Заголовок">
+        <app-input v-model="localAgendaItem.title" />
+      </form-group>
+
+      <form-group label="Описание">
+        <app-input v-model="localAgendaItem.description" multiline rows="3" />
+      </form-group>
+    </template>
+
+    <template v-else-if="localAgendaItem.type == 'talk'">
+      <form-group label="Тема">
+        <app-input v-model="localAgendaItem.title" />
+      </form-group>
+
+      <form-group label="Докладчик">
+        <app-input v-model="localAgendaItem.speaker" />
+      </form-group>
+
+      <form-group label="Описание">
+        <app-input v-model="localAgendaItem.description" multiline rows="3" />
+      </form-group>
+
+      <form-group label="Язык">
+        <select v-model="localAgendaItem.language" class="form-control">
+          <option v-for="(language, index) in talkLanguages" :key="index" :value="language.value">
+            {{ language.text }}
+          </option>
+        </select>
+      </form-group>
+    </template>
+
+    <template v-else>
+      <form-group label="Нестандартный текст (необязательно)">
+        <app-input />
+      </form-group>
+    </template>
   </div>
 </template>
 
 <script>
 import AppIcon from './AppIcon';
+import FormGroup from './FormGroup.vue';
+import AppInput from './AppInput.vue';
+import DateInput from './DateInput';
 
 const getAgendaItemTypes = () => [
   { value: 'registration', text: 'Регистрация' },
@@ -64,7 +89,89 @@ const getTalkLanguages = () => [
 export default {
   name: 'MeetupAgendaItemForm',
 
-  components: { AppIcon },
+  components: { AppIcon, FormGroup, AppInput, DateInput },
+  props: {
+    agendaItem: {
+      required: true,
+      type: Object,
+    },
+  },
+
+  data() {
+    return {
+      agendaItemTypes: getAgendaItemTypes(),
+      talkLanguages: getTalkLanguages(),
+      localAgendaItem: {
+        ...this.agendaItem,
+      },
+
+      durationOfAgenda: 0,
+    };
+  },
+
+  computed: {
+    startsAt: {
+      get() {
+        return this.localAgendaItem.startsAt;
+      },
+
+      set(newVal) {
+        this.localAgendaItem.startsAt = newVal;
+
+        //if (this.durationOfAgenda == 0) return;
+
+        let startsAtHH = +this.localAgendaItem.startsAt.split(':')[0];
+        let startsAtMM = this.localAgendaItem.startsAt.split(':')[1];
+
+        let endsAtHH = startsAtHH + this.durationOfAgenda;
+
+        if (endsAtHH > 23) endsAtHH = endsAtHH - 24;
+
+        if (endsAtHH < 10) {
+          this.localAgendaItem.endsAt = '0' + endsAtHH + ':' + startsAtMM;
+        } else {
+          this.localAgendaItem.endsAt = endsAtHH + ':' + startsAtMM;
+        }
+      },
+    },
+
+    endsAt: {
+      get() {
+        return this.localAgendaItem.endsAt;
+      },
+
+      set(newVal) {
+        this.localAgendaItem.endsAt = newVal;
+        this.durationOfAgenda = this.setDurationOfAgenda();
+      },
+    },
+  },
+
+  watch: {
+    localAgendaItem: {
+      deep: true,
+      handler(newVal) {
+        this.$emit('update:agendaItem', { ...newVal });
+      },
+    },
+  },
+
+  created: function () {
+    this.durationOfAgenda = this.setDurationOfAgenda();
+  },
+
+  methods: {
+    setDurationOfAgenda() {
+      let startsAtHH = +this.localAgendaItem.startsAt.split(':')[0];
+      let endsAtHH = +this.localAgendaItem.endsAt.split(':')[0];
+
+      if (endsAtHH >= startsAtHH) {
+        return endsAtHH - startsAtHH;
+      } else {
+        return 24 + endsAtHH - startsAtHH;
+      }
+    },
+  },
 };
 </script>
 
