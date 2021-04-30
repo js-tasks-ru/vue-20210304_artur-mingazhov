3ea2cb941b56ac044f3315319cfc569d55702938
Artur Mingazhov <arthurming7@gmail.com>
2021-04-30 12:02:07 +0500

    add solution for 09-scoped-slots/02-AppPagination

--- 09-scoped-slots/02-AppPagination/components/AppPagination.vue
+++ 09-scoped-slots/02-AppPagination/components/AppPagination.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    <!-- Решение -->
+    <slot v-for="item in currentPage" :item="item" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'AppPagination',
+  props: {
+    page: {
+      type: Number,
+    },
+    perPage: {
+      type: Number,
+    },
+    items: {
+      type: Array,
+    },
+  },
+  computed: {
+    currentPage() {
+      return this.items.slice(this.perPage * (this.page - 1), this.perPage * this.page);
+    },
+  },
 };
 </script>
 
