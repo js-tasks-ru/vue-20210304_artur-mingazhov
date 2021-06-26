1d7c1fee3b66224242ccb844cb9cbd026d38cdd6
Artur Mingazhov <arthurming7@gmail.com>
2021-06-26 07:45:27 +0300

    add solution for QuerySync

--- 04-spa/06-QuerySync/views/QuerySync.vue
+++ 04-spa/06-QuerySync/views/QuerySync.vue
@@ -1,13 +1,60 @@
 <template>
-  <meetups-view />
+  <meetups-view v-bind.sync="params" />
 </template>
 
 <script>
 import MeetupsView from '../components/MeetupsView';
-
 export default {
   name: 'QuerySync',
-
   components: { MeetupsView },
+
+  data() {
+    return {
+      params: {
+        view: this.$route.query.view,
+        date: this.$route.query.date,
+        participation: this.$route.query.participation,
+        search: this.$route.query.search,
+      },
+    };
+  },
+
+  watch: {
+    params: {
+      deep: true,
+      handler() {
+        const { view, date, participation, search } = this.params;
+
+        this.$router
+          .push({
+            query: {
+              view: view === 'list' ? undefined : view,
+              date: date === 'all' ? undefined : date,
+              participation: participation === 'all' ? undefined : participation,
+              search: search === '' ? undefined : search,
+            },
+          })
+          .catch((error) => {
+            if (error.name === 'NavigationDuplicated') {
+              return this.currentRoute;
+            } else {
+              throw error;
+            }
+          });
+      },
+    },
+    $route: {
+      handler() {
+        const { view, date, participation, search } = this.$route.query;
+
+        this.params = {
+          view: view === undefined ? 'list' : view,
+          date: date === undefined ? 'all' : date,
+          participation: participation === undefined ? 'all' : participation,
+          search: search === undefined ? '' : search,
+        };
+      },
+    },
+  },
 };
 </script>
